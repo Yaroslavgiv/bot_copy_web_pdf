9c68f1296f79313f1ee24e47d7b525c12d355bb2
Yaroslav <yaroslavgiv31@gmail.com>
2023-08-24 10:10:41 +0300

    push

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import re
+from slugify import slugify
+import telebot
+import  requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pdf_generator import PdfGenerator
 
+# Создаем экземпляр бота
+bot = telebot.TeleBot('Your Token')
+# Функция, обрабатывающая команду /start
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Присылай ссылку )')
 
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    try:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        page_content = requests.get(url=message.text).text
+
+        name_st = re.search(r".*?<title>(.*?)</title>.*", page_content)[0]
+        name_st = slugify(name_st.strip()[7:-8], allow_unicode=True)
+
+        pdf_file = PdfGenerator([message.text]).main()
+        with open(f'C:\\Users\{name_st}.pdf', "wb") as outfile:
+            outfile.write(pdf_file[0].getbuffer())
+
+    except BaseException as e:
+        bot.send_message(message.chat.id, e)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
+
